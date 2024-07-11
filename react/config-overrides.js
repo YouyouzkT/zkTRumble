3a67cffef91be82663c4eaa3bc78d60baefdf874
YouyouzkT <usul_le_fremen@yahoo.fr>
2024-07-11 21:38:05 +0200

    Add React version

---
+++ react/config-overrides.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser'),
+    url: require.resolve('url')
+  };
+
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ];
+
+  return config;
+};
